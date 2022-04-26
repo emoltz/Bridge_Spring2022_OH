5406f95ea134bec5762fcf62703807fb8588a5e1
emoltz <es5541@nyu.edu>
2022-04-26 14:44:21 -0400

    for loop example

--- helloworld.cpp
+++ helloworld.cpp
@@ -2,9 +2,18 @@
 using namespace std;
 
 int main(){
-    double x = 5.9;
-    int i = (int)x;
-    cout << i;
+    bool end = true;
+    int i = 1;
+    while(end){
+        cout << i << endl;
+        i++;
+        if (i == 15){
+            end = false;
+        }
+        if (end == false){
+            cout << "Hello!";
+        }
+    }
 
     return 0;
 }
