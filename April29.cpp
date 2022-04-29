4eba4f9e2e55a42211a2d83d6c02b576470a3ae6
emoltz <es5541@nyu.edu>
2022-04-29 15:32:08 -0400

    pass by ref

---
+++ April29.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+
+void multiplyByFive(int &x){
+    x = x * 5;
+}
+
+int multiplyByFive_withReturn(int x){
+    int n = x * 5;
+    return n;
+}
+
+
+int multiplyByFive_withReturn(int x, bool &awesomeness){
+    int n = x * 5;
+
+    if (awesomeness == false){
+        awesomeness = true;
+    }
+    else{
+        awesomeness = false;
+    }
+
+    return n;
+}
+
+
+int main(){
+    
+    int i = 10;
+    bool Ethan = false;
+
+    int result = multiplyByFive_withReturn(i, Ethan);
+    cout << result << endl;
+    
+    cout << "Ethan is awesome? ";
+    if (Ethan == true){
+        cout << "Yes!";
+    }
+    else{
+        cout << "No :(";
+    }
+
+
+    return 0;
+}
--- test.cpp
+++ test.cpp
@@ -3,5 +3,6 @@ using namespace std;
 
 int main(){
     cout << "Github test!";
+    
     return 0;
 }
