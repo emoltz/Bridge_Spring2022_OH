85d92d1a4b97cab5194d89018d4019ebbdd6edf6
emoltz <es5541@nyu.edu>
2022-05-19 11:07:57 -0400

    double pointers

--- passbyref.cpp
+++ passbyref.cpp
@@ -56,7 +56,21 @@ void dynamicArrays(){
 }
 
 int main(){
-   
+   int *arr = new int[6];
+   for (int i = 0; i < 6; i++){
+       arr[i] = i;
+   }
+
+   int **pointer = &arr;
+
+   cout << **pointer;
+   cout << **pointer;
+
+   //print
+//    for (int i = 0; i < 6; i++){
+//        cout << arr[i] << " ";
+//    }
+
 
     return 0;
 
