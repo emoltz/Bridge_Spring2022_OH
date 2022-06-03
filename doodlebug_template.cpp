3b330b82311489ce6dace92ba065ee18690affe2
emoltz <es5541@nyu.edu>
2022-06-03 16:06:20 -0400

    June3

--- doodlebug_template.cpp
+++ doodlebug_template.cpp
@@ -7,12 +7,13 @@ class Doodlebug;
 
 class Area{
 public:
-    Organism* garden[100][100];
+    Organism* garden[20][20];
 };
 
 class Organism{
 public:
     Area* world;
+    //virtual function: what functions MUST you have for the Ants and the Doodlebugs?
 };
 
 class Ant : public Organism{
