8fe2bca93042b8d4e66f27b901b735d7780afde7
emoltz <es5541@nyu.edu>
2022-06-10 15:28:23 -0400

    linkedLists

---
+++ June10.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+using namespace std;
+
+class Node{
+public: 
+    int data;
+    Node* next;
+    Node(){
+        data = 0;
+        next = nullptr;
+    }
+};
+
+class LinkedList{
+private:
+    Node* head;
+    Node* tail;
+public:
+    LinkedList(){
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    void addNode(int n){ //O(1)
+        //create a new node
+        //link it in the right place
+
+        Node *temp = new Node;
+        temp->data = n;
+        temp->next = nullptr;
+
+        //case 1: there is no list!
+        if (head == nullptr){
+            head = temp;
+            tail = temp; // if there's only 1 node, then it is both the head AND the tail
+        }
+        else{ //case 2: there is a list already
+            tail->next = temp;
+            tail = tail->next; //nullptr
+        }
+    }
+
+    void addNode_noTail(int n){ //O(n)
+        Node* temp = new Node;
+        temp->data = n;
+        temp->next = nullptr;
+
+        if (head == nullptr){
+            head = temp;
+        }
+        else{
+            //we need to iterate through the linked list until we get to the end!
+            Node* itr = head;
+            while(itr->next != nullptr){
+                itr = itr->next;
+            }
+            itr->next = temp;
+        }
+    }
+
+    void remove(int x){
+        Node* temp = head;
+        while(temp != nullptr){
+            if(temp->data == x){
+                //CHALLENGE! How to remove the node?
+            }
+            
+            temp = temp->next;
+        }
+
+    }
+
+    void display(){
+        Node* temp = head;
+        int i = 1;
+        while (temp != nullptr){
+            cout << i << ": "<<  temp->data << endl;
+            temp = temp->next;
+            i++;
+        }
+    }
+
+    
+};
+
+
+int main(){
+    LinkedList l;
+    l.addNode(5);
+    l.addNode(10);
+    l.addNode(110);
+    l.display();
+    
+    
+    
+    return 0;
+}
